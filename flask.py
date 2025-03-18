1fbc0ed9ca9f5f32ba61988dbdaae1693c6e1be3
Snowfl1ke <154212875+Snowfl1ke@users.noreply.github.com>
2025-03-18 19:53:51 +0100

    Create flask.py

---
+++ flask.py
@@ -0,0 +1,22 @@
+app = Flask(name)
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        s.connect(('10.254.254.254', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
+@app.route('/')
+def home():
+    return 'Dit is een test'
+
+if name == 'main':
+    ip_address = get_ip()
+    print(f"Flask is running on {ip_address}:5000")
+    app.run(host=ip_address, port=5000) 
